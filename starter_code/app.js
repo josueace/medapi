74c5f13b52ca9154fa391d3818ff51d7838183d0
josueacev2 <josueacev2@gmail.com>
2019-10-17 09:11:46 -0400

    cors

--- starter_code/app.js
+++ starter_code/app.js
@@ -35,13 +35,10 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
-const cors = require('cors');
-
-app.use(cors());
 
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000']
+  origin: ['https://medicrecord.herokuapp.com']
 }));
 
 // Middleware Setup
